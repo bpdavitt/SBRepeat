1062db215c5c318473dc074f846f922884a8b60c
Brendan <bpdavitt@gmail.com>
2019-08-07 12:39:43 -0500

    Add testing routes for server-DB connection; likely an issue with DB connection

--- database/index.js
+++ database/index.js
@@ -1,8 +1,37 @@
 const mongoose = require('mongoose');
 const config = require('../config.js')
 
-mongoose.connect(config.DBURI, {user: config.DBUSER, pass: config.DBPASS, useNewUrlParser: true}, ()=> {
+mongoose.connect(config.DBURI, { user: config.DBUSER, pass: config.DBPASS, useNewUrlParser: true }, () => {
   console.log('Database connected')
 })
 
-module.exports = {};
+const workoutSchema = mongoose.Schema({
+  name: String,
+  duration: Number,
+  distance: Number,
+  distanceUnit: String,
+  notes: String,
+  type: String,
+  date: { type: Date, default: Date.now }
+})
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+const getSome = () => {
+  return Workout.find().limit(100)
+    .catch(() => { console.log('Error getting some products') })
+}
+
+const addOne = (workout) => {
+  console.log(workout)
+  return Workout.updateOne({ "_id": workout._id }, workout, { upsert: true })
+    .then(result => {
+      console.log(result);
+      return result
+    })
+    .catch(err => {
+      console.log('Error:', err)
+    })
+}
+
+module.exports = { getSome, addOne };
--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const db = require('../database/index.js')
+const db = require('../database/index.js');
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Get Ready to Workout!');
 });
+
+app.post('/workouts', (req, res) => {
+  db.addOne(req.body)
+    .then((data) => {
+      console.log(data)
+      res.send('Product added')
+    })
+    .catch(()=> res.end())
+})
+
+app.get('/workouts', (req, res) => {
+  db.getSome()
+    .then(data => {
+      res.send(data);
+    })
+})
 
 app.listen(PORT, () => console.log('SBRepeat server running on port', PORT))
