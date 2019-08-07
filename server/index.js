54008d5dee2e9e301bd4ed63a838738afc26ac28
Brendan <bpdavitt@gmail.com>
2019-08-07 10:51:56 -0500

    Initial webpack/package setup; Hello World rendered with React

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Get Ready to Workout!');
+});
+
+app.listen(PORT, () => console.log('SBRepeat server running on port', PORT))
