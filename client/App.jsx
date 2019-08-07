8755bf564792b267edd9982dcd79678003dbcfac
Brendan <bpdavitt@gmail.com>
2019-08-07 16:01:27 -0500

    App now pulls last 100 workouts on load, workouts are seperated into completed and planned

--- client/App.jsx
+++ client/App.jsx
@@ -1,19 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import {Container, Row, Col} from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
+import axios from 'axios';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      completed: [],
+      planned: [],
+      processed: {}
+    }
+  }
+
+  processData(data) {
+    let completed = [];
+    let planned = [];
+    // let processed = { ...this.state.processed };
+    let processed = {}
+    for (let workout of data) {
+      if (!processed[workout._id]) {
+        processed[workout._id] = true;
+        if (workout.completed) {
+          completed.push(workout);
+        } else {
+          planned.push(workout);
+        }
+      }
+    }
+    this.setState({
+      completed: [...this.state.completed, ...completed],
+      planned: [...this.state.planned, ...planned],
+      processed: processed
+    });
+  }
+
+  getData() {
+    axios.get('/workouts')
+      .then(result => {
+        console.log(result.data)
+        this.processData(result.data);
+      })
+      .catch(err => {
+        console.log("Error getting data", err)
+      })
+  }
+
+  componentDidMount() {
+    this.getData();
   }
 
   render() {
     return (
       <Container>
         <Row id="banner">
-        <div>SBRepeat has mounted</div>
+          <div>SBRepeat has mounted</div>
         </Row>
         <Row>
           <Col id="planned-col">Planned Workouts</Col>
@@ -24,4 +66,4 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
